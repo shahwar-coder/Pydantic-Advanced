b9b75ef291f9321f4a27378bb747c45b4aab5ebc
shahwar-coder <naqvishahwar120@gmail.com>
2026-04-08 13:48:34 +0530

    pydantic first code

--- 01_Pydantic_in_Nutshell/03_First_pydantic_model.py
+++ 01_Pydantic_in_Nutshell/03_First_pydantic_model.py
@@ -2,7 +2,22 @@ import pydantic
 
 # print(pydantic.VERSION) # 2.12.5
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class User(BaseModel):
-    pass
+    name: str
+    age: int
+    email: EmailStr
+
+user = User(name="Rahul", age=54, email="rahul@gmail.com")
+
+print(f"User Object : {user}")
+print(f"User Name : {user.name}")
+print(f"User Age : {user.age}")
+print(f"User Email : {user.email}")
+
+
+# User Object : name='Rahul' age=54 email='rahul@gmail.com'
+# User Name : Rahul
+# User Age : 54
+# User Email : rahul@gmail.com
